3ef2da82bd2e5f2845f41be0017f7f14229699a0
Joao Marcos <joaomoura002@outlook.com>
2023-10-03 21:14:43 -0300

    Atualização de informações que apareceram no console.

--- src/PraticaHeranca/Main.java
+++ src/PraticaHeranca/Main.java
@@ -7,19 +7,25 @@ public class Main {
 		
 		Gerente gerente = new Gerente("Davi Miguel", 7580.0, "davimig01", "davimig112358");	
 		
-			System.out.println("Gerente: ");
-			System.out.println(gerente.getNome() + "\n" + gerente.getSalario() + "\n" + gerente.getLogin() + "\n" + gerente.getSenha() + "\n");
-			
-		Secretaria secretaria = new Secretaria("Maria Antonieta", 1500.0, "25556");
+		System.out.println("Gerente: ");
+		System.out.println( "Nome: " + gerente.getNome() + "\n" + 
+							"Salario: " + gerente.getSalario() + "\n" + 
+							"Login: " + gerente.getLogin() + "\n" + 
+							"Senha: " + gerente.getSenha() + "\n");
 		
-			System.out.println("Secretaria: ");
-			System.out.println(secretaria.getNome() + "\n" + secretaria.getSalario() + "\n" + secretaria.getRamal() + "\n");
+	Secretaria secretaria = new Secretaria("Maria Antonieta", 1500.0, "25556");
+	
+		System.out.println("Secretaria: ");
+		System.out.println( "Nome: " + secretaria.getNome() + "\n" + 
+							"Salario: " + secretaria.getSalario() + "\n" + 
+							"Ramal: " + secretaria.getRamal() + "\n");
+	
+	Telefonista telefonista = new Telefonista("Angelica Miguelho", 1500.0, "4002-8922");
 		
-		Telefonista telefonista = new Telefonista("Angelica Miguelho", 1500.0, "4002-8922");
-			
-			System.out.println("Telefonista: ");
-			System.out.println(telefonista.getNome() + "\n" + telefonista.getSalario() + "\n" + telefonista.getCodigo() + "\n");
-			
+		System.out.println("Telefonista: ");
+		System.out.println( "Nome: " + telefonista.getNome() + "\n" + 
+							"Salario: " + telefonista.getSalario() + "\n" + 
+							"Codigo: " + telefonista.getCodigo() + "\n");
 			
 		
 
